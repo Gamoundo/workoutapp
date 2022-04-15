36d058832b673dc11cea6bf310730a19f62a71cd
Akil Griffiths <akilgg@gmail.com>
2022-04-14 21:28:32 -0400

    Setup the routes
    functioning navbar

--- flex/src/App.js
+++ flex/src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
-import { useRoutes } from "react-router-dom"
+import { useRoutes } from "react-router-dom";
+import Navbar from './Navbar';
+import Excercises from './Excercises';
+import Routines from './Routines';
+import Stretches from './Stretches'
+import Home from './Home'
 import './App.css';
 
 function App() {
+
+  const routes = useRoutes(
+    [{ path: '/', element: <Home /> },
+    { path: `/exercises`, element: <Excercises /> },
+    { path: '/routines', element: <Routines/> },
+    { path: '/stretches', element: <Stretches /> }
+    
+  ])
+
   return (
     <div className="App">
       <h1>Let's Get Better</h1>
+      <div>
+        <Navbar />
+      </div>
+      {routes}
     </div>
-  );
+  )
 }
 
 export default App;
